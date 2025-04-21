94188c329a37dded2dd7b68c3378807ac17fb551
prc7558 <parthchaudhari2024.comp@mmcoe.edu.in>
2025-04-22 01:30:48 +0530

    old postcss
    old postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-import typography from "@tailwindcss/typography";
-import animate from "tailwindcss-animate";
-
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./client/**/*.{js,ts,jsx,tsx}",
-    "./shared/**/*.{js,ts,jsx,tsx}",
-    "./server/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  plugins: [typography, animate],
 }
