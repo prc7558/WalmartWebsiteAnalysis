57c84281d80b7a32da5b5c60beed2f3147975795
prc7558 <parthchaudhari2024.comp@mmcoe.edu.in>
2025-04-22 00:52:50 +0530

    postcss update
    postcss update

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: [
+    "./client/**/*.{js,ts,jsx,tsx}",
+    "./shared/**/*.{js,ts,jsx,tsx}",
+    "./server/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
+  plugins: [],
 }
